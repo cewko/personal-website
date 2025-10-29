cddbf0d4d77dcb5abe61b733dca235554da72b31
cewko <221901489+cewko@users.noreply.github.com>
2025-10-29 01:42:28 +0100

    refactor(hangout): remove blocking redis call from hangout for faster page load

--- apps/hangout/templatetags/hangout_tags.py
+++ apps/hangout/templatetags/hangout_tags.py
@@ -7,11 +7,4 @@ register = template.Library()
 
 @register.inclusion_tag('hangout/hangout_widget.html')
 def hangout_widget():
-    try:
-        redis_client = get_sync_redis_client()
-        online_count = redis_client.scard("online_users")
-    except Exception as e:
-        print(f"Redis error: {e}")
-        online_count = 0
-    
-    return {"online_count": online_count}
+    return {"online_count": "..."}
